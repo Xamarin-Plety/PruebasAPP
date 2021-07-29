1185aaec053cd8f78442931b519e811b2dc3bf94
Xamarin-Plety <78217024+Xamarin-Plety@users.noreply.github.com>
2021-07-29 14:49:58 -0500

    Restore

--- PruebasAPP/PruebasAPP/PruebasAPP/App.xaml.cs
+++ PruebasAPP/PruebasAPP/PruebasAPP/App.xaml.cs
@@ -1,8 +1,6 @@
-﻿using PruebasAPP.Models;
-using PruebasAPP.Services;
+﻿using PruebasAPP.Services;
 using PruebasAPP.Views;
 using System;
-using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,31 +8,13 @@ namespace PruebasAPP
 {
     public partial class App : Application
     {
-        public static App Current;
-        public static int iIdEmpleado { get; set; }
-        public static int iIdEmpresa { get; set; }
-        public static MemoryStream msFoto { get; set; }
-        public static cEmpleado oEmpleado { get; set; }
 
         public App()
         {
             InitializeComponent();
 
-            Current = this;
             DependencyService.Register<MockDataStore>();
-
-            if (App.Current.Properties.ContainsKey("IsLoggedIn"))
-            {
-                if ((bool)App.Current.Properties["IsLoggedIn"])
-                {
-                    MainPage = new AppShell();
-                }
-                else
-                {
-                    MainPage = new LoginPage();
-                }
-            }
-
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
